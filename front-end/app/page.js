1918907e44077863be6f92a7b449a3c3d457f4c0
Giridhar <giridhaar1105@gmail.com>
2025-02-06 23:56:48 +0530

    added or changed many

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,18 +1,11 @@
-import Image from "next/image";
-import Signup from "./Signup/page";
 import Header from "./Header/page";
-import Sidebar from "./Sidebar/page";
 import Dashboard from "./dashboard/page";
-import Fraud from "./fraud-alerts/page";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      {/* <Sidebar /> */}
 ..      <Dashboard />
-      {/* <Fraud /> */}
-      {/* <Footer /> */}
     </div>
   );
 }
